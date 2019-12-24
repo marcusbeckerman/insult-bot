6b2abe7f71eac6985ea90f180e15d5487423bb8e
marcusbeckerman <48850621+marcusbeckerman@users.noreply.github.com>
2019-12-24 03:12:25 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,26 +4,36 @@ import time
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix = "!")
-TOKEN = "NjU4OTU4NTcyMzA2MTY5ODY2.XgHVBA.9iR_qVox2sN8OYS30G6QV5whoeA"
+TOKEN = "NjU4OTU4NTcyMzA2MTY5ODY2.XgHufQ.OoATTraBRDAXdNnXVhh1HJu4JiM"
 
+
+def get_insult():
+    f = open("insults.json")
+    insults = eval(str(f.read()))
+    return random.choice(insults)
 
 async def insult_func(ctx,interval):
     members = ctx.guild.members
-    f = open("insults.json")
-    insults = eval(str(f.read()))
     while True:
-        insult = random.choice(insults)
+        insult = get_insult()
         user = random.choice(members)
         while user.bot == True:
             user = random.choice(members)
         await ctx.send(user.mention + ", " + insult.lower())
         time.sleep(int(interval))
 
+
 @bot.command()
-async def insult(ctx, interval):
+async def insult_timer(ctx, interval):
     '''insults a random person at an interval in seconds'''
     await ctx.send("Sending an insult every " + interval + " seconds.")
     await insult_func(ctx, interval)
 
+@bot.command()
+async def insult(ctx, *, name):
+    list = ctx.guild.members
+    for member in list:
+        if member.display_name.lower() == name.lower():
+            await ctx.send(member.mention + ", " + get_insult().lower())
 
 bot.run(TOKEN)
